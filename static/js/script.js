1589fffa04a18b83ea9c7d827ef38ed16e7665e0
Jegouar <pauljegou1974@gmail.com>
2021-01-15 02:13:39 +0000

    Create matches accordion

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger icon */
+
 function reveal() {
     var x = document.getElementById("links");
     if (x.style.display === "block") {
@@ -6,5 +7,25 @@ function reveal() {
     } 
     else {
         x.style.display = "block";
+    }
+}
+
+
+/* Accordion */
+
+let accordionItem = document.getElementsByClassName('accordionItem');
+let accordionItemHeading = document.getElementsByClassName('accordionItemHeading');
+
+for (i = 0; i < accordionItemHeading.length; i++) {
+    accordionItemHeading[i].addEventListener('click', toggleItem, false);
+}
+
+function toggleItem() {
+    let itemClass = this.parentNode.className;
+    for (i = 0; i < accordionItem.length; i++) {
+        accordionItem[i].className = 'accordionItem close';
+    }
+    if (itemClass == 'accordionItem close') {
+        this.parentNode.className = 'accordionItem open';
     }
 }
